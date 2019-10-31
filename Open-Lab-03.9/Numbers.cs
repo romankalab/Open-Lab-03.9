6dda082d32bfe1caddf24a5994c83c320659fc47
romankalab <roan.kalab@mail.com>
2019-10-31 15:19:24 +0100

    Initial

---
+++ Open-Lab-03.9/Numbers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PrimeNumbers
+{
+    class Numbers
+    {
+        public bool IsPrimeNumber(int number)
+        {
+            if (number < 2)
+                return false;
+            if (number == 2)
+                return true;
+            for (var i = 2; i < number; i++)
+                if (number % i == 0)
+                    return false;
+            return true;
+        }
+    }
+}
---
+++ Open-Lab-03.9/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using PrimeNumbers;
+
+namespace Open_Lab_03._9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number:");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            Numbers numbers = new Numbers();
+            bool result = numbers.IsPrimeNumber(number);
+
+            Console.WriteLine($"{result}");
+        }
+    }
+}
